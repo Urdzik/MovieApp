a56034fc625855edb845d247ac47087cd1f9a9a9
Slavik Urdzik <urdziksv@gmail.com>
2020-01-19 20:07:16 +0200

    Добавлен кастомный Toolbar Добавлена тёмная тема

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
+        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 }
