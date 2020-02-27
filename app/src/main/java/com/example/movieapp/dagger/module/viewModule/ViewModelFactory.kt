57fb463691f3b0bc5699ce710210c4da3fdf7259
Slavik Urdzik <Urdziksv@gmail.com>
2020-02-27 07:17:11 +0200

    Added viewModelFactory, ViewModelModule and annotation class

--- app/src/main/java/com/example/movieapp/dagger/module/viewModule/ViewModelFactory.kt
+++ app/src/main/java/com/example/movieapp/dagger/module/viewModule/ViewModelFactory.kt
@@ -1,4 +1,17 @@
 package com.example.movieapp.dagger.module.viewModule
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory @Inject constructor(private val viewModel: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        val viewModelProvider = viewModel[modelClass]
+            ?: throw IllegalArgumentException(" model class $modelClass not found")
+        return viewModelProvider.get() as T
+    }
+
 }
