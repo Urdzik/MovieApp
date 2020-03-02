fb69f2f115e60a5043ad52e18e717723b92c0218
Slavik Urdzik <Urdziksv@gmail.com>
2020-03-02 18:09:47 +0200

    Changed text size in SplashActivity and added night theme in it

--- app/src/main/java/com/example/movieapp/utils/SplashActivity.kt
+++ app/src/main/java/com/example/movieapp/utils/SplashActivity.kt
@@ -1,20 +1,30 @@
 package com.example.movieapp.utils
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movieapp.MainActivity
 import com.example.movieapp.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_NO -> iv.setImageResource(R.drawable.day)
+            Configuration.UI_MODE_NIGHT_YES -> iv.setImageResource(R.drawable.night)
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> iv.setImageResource(R.drawable.day)
+        }
+
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 1000L)
+        }, 1500L)
     }
 }
