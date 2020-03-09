5784aaa4d0c24e592643f2f2eeda7bbd3cda521c
Slavik Urdzik <Urdziksv@gmail.com>
2020-03-09 19:52:34 +0200

    Bug fix

--- app/src/main/java/com/example/movieapp/dagger/module/NetworkModule.kt
+++ app/src/main/java/com/example/movieapp/dagger/module/NetworkModule.kt
@@ -15,6 +15,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.io.File
 import java.util.*
 import java.util.concurrent.TimeUnit
--- app/src/main/java/com/example/movieapp/model/network/data/PhotoId.kt
+++ app/src/main/java/com/example/movieapp/model/network/data/PhotoId.kt
@@ -5,7 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class PhotoId(
     @SerializedName("results") val small: List<Small>,
     val page: Int
-
 )
 
 data class Small(
