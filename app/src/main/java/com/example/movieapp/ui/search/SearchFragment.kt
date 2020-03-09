1420f653e768f3a9d508b2ba46cd4c36b392aaed
Slavik Urdzik <Urdziksv@gmail.com>
2020-03-09 21:38:26 +0200

    Added search and List Fragment

---
+++ app/src/main/java/com/example/movieapp/ui/search/SearchFragment.kt
@@ -0,0 +1,33 @@
+package com.example.movieapp.ui.search
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.movieapp.R
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    private lateinit var viewModel: SearchViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
