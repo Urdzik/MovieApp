6308340bf89531d4c84681d18080f73e1d312627
Slavik Urdzik <Urdziksv@gmail.com>
2020-03-19 10:04:18 +0200

    Changed List Activity to Fragment

--- app/src/main/java/com/example/movieapp/dagger/component/AppComponent.kt
+++ app/src/main/java/com/example/movieapp/dagger/component/AppComponent.kt
@@ -3,7 +3,7 @@ package com.example.movieapp.dagger.component
 import com.example.movieapp.dagger.module.NetworkModule
 import com.example.movieapp.dagger.module.viewModule.ViewModelModule
 import com.example.movieapp.ui.detail.DetailFragment
-import com.example.movieapp.ui.list.ListActivity
+import com.example.movieapp.ui.list.ListFragment
 import com.example.movieapp.ui.overview.OverviewFragment
 import dagger.Component
 import javax.inject.Singleton
@@ -12,6 +12,6 @@ import javax.inject.Singleton
 @Component(modules = [NetworkModule::class, ViewModelModule::class])
 interface AppComponent{
     fun inject(overviewFragment: OverviewFragment)
-    fun inject(detailActivity: DetailFragment)
-    fun inject(listActivity: ListActivity)
+    fun inject(detailFragment: DetailFragment)
+    fun inject(listFragment: ListFragment)
 }
