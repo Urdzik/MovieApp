13f5534f16dff717dbe648a1037d5a668c328372
Slavik Urdzik <urdziksv@gmail.com>
2020-05-04 09:07:58 +0300

    Added viewModel for User Fragment

--- app/src/main/java/com/example/movieapp/ui/profile/user/UserFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/profile/user/UserFragment.kt
@@ -1,18 +1,18 @@
 package com.example.movieapp.ui.profile.user
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.movieapp.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.movieapp.databinding.UserFragmentBinding
-
 
 
 class UserFragment : Fragment() {
 
     lateinit var binding: UserFragmentBinding
+    lateinit var viewModel: UserViewModel
 
 
     override fun onCreateView(
@@ -21,9 +21,14 @@ class UserFragment : Fragment() {
     ): View? {
         binding = UserFragmentBinding.inflate(layoutInflater)
 
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
         val args = UserFragmentArgs.fromBundle(requireArguments()).thisUser
 
+        viewModel.getUser(args)
 
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
         return binding.root
     }
 
--- app/src/main/java/com/example/movieapp/ui/profile/user/UserViewModel.kt
+++ app/src/main/java/com/example/movieapp/ui/profile/user/UserViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.movieapp.ui.profile.user
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
 
 class UserViewModel : ViewModel(){
+
+    private var _currentUser = MutableLiveData<FirebaseUser>()
+    val currentUser: LiveData<FirebaseUser>
+        get() = _currentUser
+
+    fun getUser(user: FirebaseUser){
+        _currentUser.value = user
+    }
 
 }
